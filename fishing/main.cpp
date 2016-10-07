6029d910b931db2bb12579c7a588a1f8f1718be1
Peter Mata <pmata@westmont.edu>
2016-10-06 19:44:28 -0700

    Initial Commit

---
+++ fishing/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  fishing
+//
+//  Created by Peter Mata on 10/6/16.
+//  Copyright © 2016 Peter Mata. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
